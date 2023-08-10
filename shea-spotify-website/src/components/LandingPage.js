1192f1dc12d9ac8e45bc9029e58fc7015f4f19ea
Shea Keogh <64904542+sheakeogh@users.noreply.github.com>
2023-08-10 14:45:34 +0100

    Update LandingPage.js

--- shea-spotify-website/src/components/LandingPage.js
+++ shea-spotify-website/src/components/LandingPage.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Button } from '@material-ui/core';
 
 import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
 
 function LandingPage() {
 
