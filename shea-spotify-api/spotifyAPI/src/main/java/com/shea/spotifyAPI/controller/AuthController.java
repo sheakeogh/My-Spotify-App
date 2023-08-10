ad4c29a1de071bde4eea4b42f23477dc3a2447ae
Shea Keogh <64904542+sheakeogh@users.noreply.github.com>
2023-08-10 14:42:57 +0100

    Refactor AuthController.java

--- shea-spotify-api/spotifyAPI/src/main/java/com/shea/spotifyAPI/controller/AuthController.java
+++ shea-spotify-api/spotifyAPI/src/main/java/com/shea/spotifyAPI/controller/AuthController.java
@@ -4,61 +4,55 @@ import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
-import com.wrapper.spotify.model_objects.specification.Artist;
-import com.wrapper.spotify.model_objects.specification.Paging;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
-import com.wrapper.spotify.requests.data.personalization.simplified.GetUsersTopArtistsRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.core5.http.ParseException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*", maxAge = 4800)
+@Slf4j
 public class AuthController {
 
-    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/api/get-user-code/");
-    public String code = "";
-
-    public static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+    public static SpotifyApi spotifyApi = new SpotifyApi.Builder()
             .setClientId("5079883d0bec4f3d8705d674439e4988")
             .setClientSecret("9b92b73c7bf44a4daa2bf435110093e3")
-            .setRedirectUri(redirectUri)
+            .setRedirectUri(SpotifyHttpManager.makeUri("http://localhost:8080/get-user-code/"))
             .build();
 
-    @GetMapping("login")
+    @GetMapping("/login")
     @ResponseBody
     public String spotifyLogin() {
         AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
                 .scope("user-read-private, user-read-email, user-top-read")
                 .show_dialog(true)
                 .build();
-        final URI uri = authorizationCodeUriRequest.execute();
-        return uri.toString();
+
+        log.debug("AuthController.spotifyLogin making URI for login :: {}", authorizationCodeUriRequest.execute().toString());
+
+        return authorizationCodeUriRequest.execute().toString();
     }
 
-    @GetMapping(value = "get-user-code")
+    @GetMapping(value = "/get-user-code")
     public void getSpotifyUserCode(@RequestParam("code") String userCode, HttpServletResponse response) throws IOException {
-        code = userCode;
-        AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code)
-                .build();
+        AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(userCode).build();
 
         try {
-            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+            AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
 
             // Set access and refresh token for further "spotifyApi" object usage
             spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
             spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
 
-            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
-        } catch (IOException | SpotifyWebApiException | org.apache.hc.core5.http.ParseException e) {
+        } catch (IOException | ParseException | SpotifyWebApiException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        System.out.println(spotifyApi.getAccessToken());
+
         response.sendRedirect("http://localhost:3000/top-artists");
-//        return spotifyApi.getAccessToken();
     }
 }
