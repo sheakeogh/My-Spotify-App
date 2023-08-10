116442e1c440b333aaf26c05d07dcd130fd19f9a
Shea Keogh <64904542+sheakeogh@users.noreply.github.com>
2023-08-10 14:46:12 +0100

    Update index.js

--- shea-spotify-website/src/index.js
+++ shea-spotify-website/src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-//import 'semantic-ui-css/semantic.min.css'
-
-
-import * as serviceWorker from "./serviceWorker";
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 ReactDOM.render(
     <Router>
@@ -18,5 +11,3 @@ ReactDOM.render(
   ,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
